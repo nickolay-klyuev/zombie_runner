ed64c5fcdbc7b1f029afe362077176ee6e3fb0eb
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-03-31 11:15:19 +0300

    add radio system

--- Assets/Scripts/SunController.cs
+++ Assets/Scripts/SunController.cs
@@ -6,14 +6,6 @@ public class SunController : MonoBehaviour
 {
     public float timeScale;
 
-    private Quaternion startRotation;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        startRotation = transform.rotation;
-    }
-
     // Update is called once per frame
     void Update()
     {
